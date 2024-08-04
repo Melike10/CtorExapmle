5fb089591efda76510da55584cb48be1634d24ae
GÖZ'SS <GZ'SS@DESKTOP-NKTDPLS>
2024-08-04 16:56:41 +0300

    Add project files.

---
+++ CtorExapmle/Baby.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtorExapmle
+{
+    internal class Baby
+    {
+        // fields
+        private string _firstName;
+        private string _lastName;
+        private DateTime _dateOfBirth;
+
+        // default constructure
+        public Baby()
+        {
+
+        }
+        // constructure with parameters
+        public Baby(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            Console.WriteLine("Ingaaa");
+            _dateOfBirth = DateTime.Now;
+        }
+
+        // properties
+        public string FirstName { get { return _firstName; } set { _firstName = value; } }
+        public string LastName { get { return _lastName; } set { _lastName = value; } }
+        public DateTime DateOfBirth { get { return _dateOfBirth; } set { _dateOfBirth = value; } }
+
+        public void IntroduceBaby()
+        {
+            Console.WriteLine("Bebeğin bilgileri --> "+FirstName+" "+LastName+ " "+DateOfBirth);
+
+        }
+
+    }
+}
---
+++ CtorExapmle/Program.cs
@@ -0,0 +1,13 @@
+﻿using CtorExapmle;
+
+// call First object using default ctor
+Baby baby1 = new Baby();
+baby1.FirstName = "Hürreycan";
+baby1.LastName = "Sezgin";
+baby1.DateOfBirth = new DateTime(2024, 08, 04);
+baby1.IntroduceBaby();
+
+// call second object using ctor with parameters
+Baby baby2 = new Baby("İpek","Nalburcuoğlu");
+baby2.IntroduceBaby();
+
